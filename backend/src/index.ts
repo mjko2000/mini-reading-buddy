3d6e3f325f790e7cf5f46e643006564c34ddc101
ndeva <ndeva3199@gmail.com>
2025-07-10 01:28:46 +0700

    backend allow all origin

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -11,8 +11,7 @@ const PORT = process.env.PORT || 3001;
 app.use(helmet());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
-    credentials: true,
+    origin: "*",
   })
 );
 app.use(morgan("combined"));
